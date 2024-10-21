69e4a8592b5beaba77b9800171fe6c78437c8df3
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-21 20:12:52 +0100

    Update easyLLM.py

--- easyLLM/easyLLM.py
+++ easyLLM/easyLLM.py
@@ -18,7 +18,7 @@ import random
 # Suppress unnecessary warnings
 hf_logging.set_verbosity_error()
 
-UNSLOTH_MODELS = ["cognitivecomputations/dolphin-2.9-llama3-8b"]
+UNSLOTH_MODELS = ["akjindal53244/Llama-3.1-Storm-8B"]
 
 class EasyLLM:
     """
