4c97842df74cf9e61f9908af8673ea453098758b
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-21 17:16:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="easyLLM",  # Name of the tool/package
+    version="0.0.1",  # Current version
+    url="https://github.com/user1342/easyLLM",
+    packages=find_packages(),  # Automatically find package directories
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: Ubuntu",
+    ],
+    install_requires=requirements,  # Dependencies from requirements.txt
+)
