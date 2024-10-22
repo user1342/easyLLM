cd525b57b4022a30ea53996d0ea8e2990757e214
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-22 18:43:11 +0100

    Update easyLLM.py

--- easyLLM/easyLLM.py
+++ easyLLM/easyLLM.py
@@ -18,7 +18,7 @@ import random
 # Suppress unnecessary warnings
 hf_logging.set_verbosity_error()
 
-UNSLOTH_MODELS = ["akjindal53244/Llama-3.1-Storm-8B"]
+UNSLOTH_MODELS = ["unsloth/Llama-3.1-Storm-8B-bnb-4bit"]
 
 class EasyLLM:
     """
