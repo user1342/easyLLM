064856acc8383f1fd8e417f651a010daf5263ded
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-14 19:49:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name="easyLLM",  # Name of the tool/package
-    version="0.0.14",  # Current version
+    version="0.0.15",  # Current version
     url="https://github.com/user1342/easyLLM",
     packages=find_packages(),  # Automatically find package directories
     classifiers=[
